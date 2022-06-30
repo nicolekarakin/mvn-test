27f498bfa50686de05938bff0e1f8ceff4eba94f
NKair <nkair@MacBook-Air.fritz.box>
2022-06-30 20:37:48 +0200

    test

--- src/main/java/org/nnn4eu/nfische/Main.java
+++ src/main/java/org/nnn4eu/nfische/Main.java
@@ -4,8 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-
-        System.out.println("Hello world!!");
         int[] ab=JavaDummy.input(2);
         System.out.println(JavaDummy.summ( ab[0], ab[1]));
         System.out.println(JavaDummy.printSumm( ab[0], ab[1]));
